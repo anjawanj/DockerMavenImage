a9b13db8d061d876e5d58e63f3f7c36e6a10f0d3
ajawanja <anupjawanjal@hotmail.com>
2018-10-11 17:49:37 +0530

    First Revision Changes

---
+++ src/main/java/com/anjawanj/DockerMavenImage/DockerMavenImageApplication.java
@@ -0,0 +1,21 @@
+package com.anjawanj.DockerMavenImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerMavenImageApplication {
+	
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMavenImageApplication.class, args);
+	}
+}
